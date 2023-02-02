c68feb61a12179d5d5d4e49b3a54e5e3cd5ba713
Alessio Buonomo <abuonomo@e3r3p6.42roma.it>
2023-02-02 16:56:08 +0100

    Added 90% of Bonus Part

---
+++ ft_lstiter.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstiter.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abuonomo <abuonomo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/02 16:52:03 by abuonomo          #+#    #+#             */
+/*   Updated: 2023/02/02 16:53:09 by abuonomo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+Iterates the list ’lst’ and applies the function
+’f’ on the content of each node.
+*/
+#include "libft.h"
+void ft_lstiter(t_list *lst, void (*f)(void *))
+{
+
+}
